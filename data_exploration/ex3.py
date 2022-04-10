b61aa2e09a07fc82ece3c0defd89c1b6d813527c
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-10 00:29:34 -0300

    ex3_1_0

---
+++ data_exploration/ex3.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+caminho_arquivo = os.path.dirname(__file__)
+caminho_novo = 'credit_data_base\credit_data.csv'
+caminho_csv = os.path.join(caminho_arquivo, caminho_novo)
+
+
+base_credit = pd.read_csv(caminho_csv)
+
+# Retorna os diferentes valores existentes na coluna default
+print(np.unique(base_credit['default']))  # 0 e 1
+
+# Porém, supunhamos que eu queira saber quantas vezes esses diferentes valores foram colocados
+print(np.unique(base_credit['default'], return_counts=True))
+
+# Gera um gráfico de barras mostrando a quantidade de cada uma das possibilidades. Nesse caso, a quantidade de 0 e 1 
+sns.countplot(x = base_credit['default'])
