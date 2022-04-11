3a4c04843c7486c7cbe76ad2afa099766ec6223c
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-11 04:46:13 -0300

    ex7_att

--- credit_data_exploration/ex7.py
+++ credit_data_exploration/ex7.py
@@ -12,7 +12,7 @@ caminho_csv = os.path.join(caminho_arquivo, caminho_novo)
 
 base_credit = pd.read_csv(caminho_csv)
 
-# Tratando dados incoerentes, forma 01:
+# Tratando dados inconsistentes, forma 01:
 # Apagar toda a coluna que possui esses dados, junto com todos os registros (inclusive os coerentes):
 base_credit_2 = base_credit.drop('age', axis=1)  # axis = 0 - Apaga as linhas / axis = 1 - Apaga as colunas
 # print(base_credit_2[base_credit_2['age'] < 0])  # Erro!!! A coluna "age" foi apagada, não é mais possível ver esses registros
