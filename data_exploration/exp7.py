9b3e991d0a88322d3cefc3733f4435291541f7ef
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-11 04:37:39 -0300

    exp7

---
+++ data_exploration/exp7.py
@@ -0,0 +1,20 @@
+from email.mime import base
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+caminho_arquivo = os.path.dirname(__file__)
+caminho_novo = 'credit_data_base\credit_data.csv'
+caminho_csv = os.path.join(caminho_arquivo, caminho_novo)
+
+
+base_credit = pd.read_csv(caminho_csv)
+
+# Tratando dados incoerentes, forma 01:
+# Apagar toda a coluna que possui esses dados, junto com todos os registros (inclusive os coerentes):
+base_credit_2 = base_credit.drop('age', axis=1)  # axis = 0 - Apaga as linhas / axis = 1 - Apaga as colunas
+# print(base_credit_2[base_credit_2['age'] < 0])  # Erro!!! A coluna "age" foi apagada, não é mais possível ver esses registros
+
+print(base_credit_2.head())  # Só possui as colunas clientid, income, loan e default
