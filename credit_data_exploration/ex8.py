f3ead86563119eed3938d6398acc097f1101aba9
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-11 04:42:47 -0300

    ex8

--- credit_data_exploration/ex8.py
+++ credit_data_exploration/ex8.py
@@ -1,3 +1,4 @@
+from email.mime import base
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -10,3 +11,9 @@ caminho_csv = os.path.join(caminho_arquivo, caminho_novo)
 
 
 base_credit = pd.read_csv(caminho_csv)
+
+# Informa todos os index existentes
+print(base_credit.index)
+
+# Informa os index dos elementos de base_credit que possuem "age" menor que 0
+print(base_credit[base_credit['age'] < 0].index)
