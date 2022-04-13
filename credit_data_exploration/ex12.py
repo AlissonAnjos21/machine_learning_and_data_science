daa4a943230c49639991abe0217153c60bd3abbd
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-13 04:07:44 -0300

    previsores

--- credit_data_exploration/ex12.py
+++ credit_data_exploration/ex12.py
@@ -11,4 +11,11 @@ caminho_csv = os.path.join(caminho_arquivo, caminho_novo)
 
 base_credit = pd.read_csv(caminho_csv)
 
+# Para predições, normalmente usá-se dois tipos de variáveis, as X que são dos previsores e as Y que são das classes/respostas
 
+# O iloc seleciona linhas e colunas do data frame
+X_base = base_credit.iloc[:, 1:4].values  # Primeiro parâmetro é referente a quais linhas pegar (nesse caso todas), já o segundo parâmetro, é relativo a quais colunas pegar (nesse caso, da 1 à 3 (4 não é incluído))
+Y_base = base_credit.iloc[:, 4].values  # Colocando a coluna 4 em uma única variável
+
+print(X_base)  # Funcionou!
+print(Y_base)  # Funcionou!
