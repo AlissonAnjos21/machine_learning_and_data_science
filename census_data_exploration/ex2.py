4de8894278d86bd3fcc945ce9e203a1bdafb8d01
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-14 03:01:58 -0300

    ex2

--- census_data_exploration/ex2.py
+++ census_data_exploration/ex2.py
@@ -11,4 +11,21 @@ caminho_csv = os.path.join(caminho_arquivo, caminho_novo)
 
 base_censo = pd.read_csv(caminho_csv)
 
+# Observando os dados por meio de representações gráficas:
+# Vendo as opções de renda e quantas pessoas responderam cada uma das opções
+print(np.unique(base_censo['income'], return_counts=True))
 
+print('\n\n\n\n\n')
+
+# Representando o dado anterior com gráficos:
+sns.countplot(base_censo['income'])  # Renda
+plt.show()
+
+plt.hist(x = base_censo['age'])  # Idade
+plt.show()
+
+plt.hist(x = base_censo['hour-per-week'])  # Horas de trabalho por semana
+plt.show()
+
+plt.hist(x = base_censo['education-num'])  # Quantidade de anos estudadando
+plt.show()
