b06ff1acaa4ea5c18e7d452645a0aaa58fe22fbd
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-14 02:45:31 -0300

    treemap_and_parallel

--- base.py
+++ base.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import os
 caminho_arquivo = os.path.dirname(__file__)
-caminho_novo = 'credit_data_base\credit_data.csv'
+caminho_novo = 'census_data_base\census.csv'
 caminho_csv = os.path.join(caminho_arquivo, caminho_novo)
 
 
-base_credit = pd.read_csv(caminho_csv)
+base_censo = pd.read_csv(caminho_csv)
