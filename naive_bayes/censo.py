01bad6706b2e1cb4d2cd122941782aef86f9a0b3
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-17 07:11:28 -0300

    base_censo

--- naive_bayes/censo.py
+++ naive_bayes/censo.py
@@ -18,3 +18,20 @@ print(x_censo_treinamento.shape, x_censo_teste.shape)  # Ok
 print(y_censo_treinamento.shape, y_censo_teste.shape)  # Ok
 
 from sklearn.naive_bayes import GaussianNB
+naive_censo = GaussianNB()
+naive_censo.fit(x_censo_treinamento, y_censo_treinamento)
+predicao = naive_censo.predict(x_censo_teste)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_censo_teste, predicao))  # 0.4767 = 47.67% de acerto (APENAS)
+
+from sklearn.metrics import classification_report
+# Informações interessantes sobre os percentuais de erros e acertos
+print(classification_report(y_censo_teste, predicao))
+
+# Vendo graficamente a representação dos erros e acertos obtidos
+from yellowbrick.classifier import ConfusionMatrix
+cm = ConfusionMatrix(naive_censo)
+cm.fit(x_censo_treinamento, y_censo_treinamento)
+cm.score(x_censo_teste, y_censo_teste)
+plt.show()
