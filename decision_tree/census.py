bef4612d6d359eb066b1fe669e0a7ef6f6714391
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-19 04:58:10 -0300

    base_census

---
+++ decision_tree/census.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+
+caminho_rota = os.path.dirname(__file__)
+caminho_arquivo = 'credit_and_census_data_base/censo.pkl'
+caminho_final = os.path.join(caminho_rota, caminho_arquivo)
+
+import pickle
+with open(caminho_final, 'rb') as f:
+    x_censo_treinamento, x_censo_teste, y_censo_treinamento, y_censo_teste = pickle.load(f)
+
+print(x_censo_treinamento.shape, y_censo_treinamento.shape)
+print(x_censo_teste.shape, y_censo_teste.shape)
