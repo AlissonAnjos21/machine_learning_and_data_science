2885c950dec934ba08b71ce8779020f6b9b1dcff
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-20 19:22:59 -0300

    decision_tree_census

--- decision_tree/census.py
+++ decision_tree/census.py
@@ -15,3 +15,21 @@ with open(caminho_final, 'rb') as f:
 
 print(x_censo_treinamento.shape, y_censo_treinamento.shape)
 print(x_censo_teste.shape, y_censo_teste.shape)
+print('\n')
+
+from sklearn.tree import DecisionTreeClassifier
+tree_censo = DecisionTreeClassifier(criterion='entropy', random_state=0)
+tree_censo.fit(x_censo_treinamento, y_censo_treinamento)
+prediction = tree_censo.predict(x_censo_teste)
+
+from sklearn.metrics import accuracy_score, classification_report
+print('PERCENTUAL DE ACERTO:')
+print(accuracy_score(y_censo_teste, prediction))  # 0.8104 = 81.04%
+print('\nINFORMAÇÕS SOBRE AS PREDIÇÕES:')
+print(classification_report(y_censo_teste, prediction))
+
+from yellowbrick.classifier import ConfusionMatrix
+cm = ConfusionMatrix(tree_censo)
+cm.fit(x_censo_treinamento, y_censo_treinamento)
+cm.score(x_censo_teste, y_censo_teste)
+plt.show()
