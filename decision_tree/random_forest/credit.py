af6f5d957b816977ffe1a455f47764d4013fa666
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-21 08:45:25 -0300

    credit_random_forest

--- decision_tree/random_forest/credit.py
+++ decision_tree/random_forest/credit.py
@@ -15,3 +15,21 @@ with open(caminho_final, 'rb') as f:
 
 print(x_credit_treinamento.shape, y_credit_treinamento.shape)
 print(x_credit_teste.shape, y_credit_teste.shape)
+
+from sklearn.ensemble import RandomForestClassifier
+random_forest_credit = RandomForestClassifier(n_estimators=40, criterion='entropy', random_state=0)
+random_forest_credit.fit(x_credit_treinamento, y_credit_treinamento)
+prediction = random_forest_credit.predict(x_credit_teste)
+
+from sklearn.metrics import accuracy_score, classification_report
+print('\nPERCENTUAL DE ACERTO:')
+print(accuracy_score(y_credit_teste, prediction))  # 0.984 = 98.4%
+
+print('\nINFORMAÇÕES SOBRE A PREDIÇÃO:')
+print(classification_report(y_credit_teste, prediction))
+
+from yellowbrick.classifier import ConfusionMatrix
+cm = ConfusionMatrix(random_forest_credit)
+cm.fit(x_credit_treinamento, y_credit_treinamento)
+cm.score(x_credit_teste, y_credit_teste)
+plt.show()
