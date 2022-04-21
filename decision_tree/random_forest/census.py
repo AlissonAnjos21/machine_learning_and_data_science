61c8ad976f2d3bc210190197dbcba7a2831aa340
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-21 09:03:09 -0300

    census_random_forest

--- decision_tree/random_forest/census.py
+++ decision_tree/random_forest/census.py
@@ -15,3 +15,20 @@ with open(caminho_final, 'rb') as f:
 
 print(x_censo_treinamento.shape, y_censo_treinamento.shape)
 print(x_censo_teste.shape, y_censo_teste.shape)
+
+from sklearn.ensemble import RandomForestClassifier
+random_forest_censo = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state = 0)
+random_forest_censo.fit(x_censo_treinamento, y_censo_treinamento)
+prediction = random_forest_censo.predict(x_censo_teste)
+
+from sklearn.metrics import accuracy_score, classification_report
+print('\nPERCENTUAL DE ACERTO:')
+print(accuracy_score(y_censo_teste, prediction))  # 0.8507 = 85.07%
+print('\nINFORMAÇÕES DA PREDIÇÃO:')
+print(classification_report(y_censo_teste, prediction))
+
+from yellowbrick.classifier import ConfusionMatrix
+cm = ConfusionMatrix(random_forest_censo)
+cm.fit(x_censo_treinamento, y_censo_treinamento)
+cm.score(x_censo_teste, y_censo_teste)
+plt.show()
