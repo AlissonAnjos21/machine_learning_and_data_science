7f61978d443ecf084b522604e361deb9964669d7
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-24 19:56:57 -0300

    credit_explication

--- majority_learner/credit.py
+++ majority_learner/credit.py
@@ -12,7 +12,8 @@ print(credit_data_base.domain)
 print('\n\n')
 
 from collections import Counter
-print(Counter(str(item.get_class()) for item in credit_data_base))  # Retorna um dicionário onde cada chave é um dos valores que aparecem e seu valor é quantas vezes o valor aparece
+# Retorna um dicionário onde cada chave é um dos valores que aparecem e seu valor é quantas vezes o valor aparece
+print(Counter(str(item.get_class()) for item in credit_data_base))  # Counter({'0': 1717, '1': 283}), ou seja, de 2000 registros (1717 + 283), 1717 estão classificados com 0
 
 print('\n\n')
 
@@ -22,4 +23,6 @@ print('\n\n')
 
 majority_learner = Orange.classification.MajorityLearner()
 not_prediction = Orange.evaluation.testing.TestOnTestData(credit_data_base, credit_data_base, [majority_learner])
+# Como a maioria dos registros são 0, os registros que seriam classificados belo Majority Learner também seriam 0
+# Como o Majority Learner classifica com base na maioria, então o seu percentual de acerto deve ser referente a quantidade de valores majoritarios de comparados a quantidade total, como a quantidade total é 2000 e a quantidade de valores majoritários é 1717, então o percentual deve ser de 1717 / 2000 (0.8585 = 85.85%)
 print(Orange.evaluation.CA(not_prediction))  # 0.8585 = 85.85%
