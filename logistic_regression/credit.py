5e81d6948b196b44e325ced5913db0a3d6d3d015
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-25 18:54:16 -0300

    import_bases

---
+++ logistic_regression/credit.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+
+dir_path = os.path.dirname(__file__)
+file_path = 'credit_and_census_data_base/credit.pkl'
+final_path = os.path.join(dir_path, file_path)
