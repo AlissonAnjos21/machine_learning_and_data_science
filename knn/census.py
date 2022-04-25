475c71ef890394ab52e37c628e11a4e4657b8f38
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-25 18:20:26 -0300

    knn_census

--- knn/census.py
+++ knn/census.py
@@ -15,3 +15,20 @@ with open(final_path, 'rb') as f:
 
 print(x_censo_training.shape, y_censo_training.shape)
 print(x_censo_test.shape, y_censo_test.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn_censo = KNeighborsClassifier(n_neighbors=10)  # No exemplo da base de dados do credit outros valores foram informados, porém todos aqueles valores já estão definidos por padrão. Nesse caso, eu alterei o número padrão de vizinhos que é 5 para 10.
+knn_censo.fit(x_censo_training, y_censo_training)
+prediction = knn_censo.predict(x_censo_test)
+
+from sklearn.metrics import accuracy_score, classification_report
+print('\nPERCENTUAL DE ACERTO:')
+print(accuracy_score(y_censo_test, prediction))  # 0.8290 = 82.90%
+print('\n\nINFORMAÇÕES DA PREDIÇÃO:')
+print(classification_report(y_censo_test, prediction))
+
+from yellowbrick.classifier import ConfusionMatrix
+cm = ConfusionMatrix(knn_censo)
+cm.fit(x_censo_training, y_censo_training)
+cm.score(x_censo_test, y_censo_test)
+plt.show()
