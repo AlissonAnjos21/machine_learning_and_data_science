ed2f6f04952540752f7d7330bbac8478b8e11c97
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-25 23:15:12 -0300

    census_logistic_regression

--- logistic_regression/census.py
+++ logistic_regression/census.py
@@ -17,3 +17,26 @@ print('\nVALORES TREINAMENTO:')
 print(x_censo_training.shape, y_censo_training.shape)
 print('VALORES TESTE:')
 print(x_censo_test.shape, y_censo_test.shape)
+
+from sklearn.linear_model import LogisticRegression
+logistic_regression_censo = LogisticRegression(random_state=0)
+logistic_regression_censo.fit(x_censo_training, y_censo_training)
+
+print('\nVALOR DE B0:')
+print(logistic_regression_censo.intercept_)
+print('\nVALORES DOS COEFICIENTES (Bs):')
+print(logistic_regression_censo.coef_)
+
+prediction = logistic_regression_censo.predict(x_censo_test)
+
+from sklearn.metrics import accuracy_score, classification_report
+print('\nPERCENTUAL DE ACERTO:')
+print(accuracy_score(y_censo_test, prediction))  # 0.8495 = 84.95%
+print('\nINFORMAÇÕES SOBRE A PREDIÇÃO:')
+print(classification_report(y_censo_test, prediction))
+
+from yellowbrick.classifier import ConfusionMatrix
+cm = ConfusionMatrix(logistic_regression_censo)
+cm.fit(x_censo_training, y_censo_training)
+cm.score(x_censo_test, y_censo_test)
+plt.show()
