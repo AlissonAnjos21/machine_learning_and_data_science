f8115de72417478611f46b8e5f2cc3c95e8f5cb5
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-25 22:56:50 -0300

    credit_logistic_regression

--- logistic_regression/credit.py
+++ logistic_regression/credit.py
@@ -17,3 +17,26 @@ print('\nVALORES TREINAMENTO:')
 print(x_credit_training.shape, y_credit_training.shape)
 print('VALORES TESTE:')
 print(x_credit_test.shape, y_credit_test.shape)
+
+from sklearn.linear_model import LogisticRegression
+logistic_regression_credit = LogisticRegression(random_state=0)
+logistic_regression_credit.fit(x_credit_training, y_credit_training)
+
+print('\nVALOR DE B0:')
+print(logistic_regression_credit.intercept_)
+print('\nVALORES DOS COEFICIENTES (Bs):')
+print(logistic_regression_credit.coef_)
+
+prediction = logistic_regression_credit.predict(x_credit_test)
+
+from sklearn.metrics import accuracy_score, classification_report
+print('\nPERCENTUAL DE ACERTO:')
+print(accuracy_score(y_credit_test, prediction))  # 0.946 = 94.60%
+print('\nINFORMAÇÕES DA PREDIÇÃO:')
+print(classification_report(y_credit_test, prediction))
+
+from yellowbrick.classifier import ConfusionMatrix
+cm = ConfusionMatrix(logistic_regression_credit)
+cm.fit(x_credit_training, y_credit_training)
+cm.score(x_credit_test, y_credit_test)
+plt.show()
