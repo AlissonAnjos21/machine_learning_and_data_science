ebf5b3086a96ec0ba3a21fb6ca5a678b6e59165c
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-25 21:05:24 -0300

    credit_base

--- logistic_regression/credit.py
+++ logistic_regression/credit.py
@@ -8,3 +8,12 @@ import os
 dir_path = os.path.dirname(__file__)
 file_path = 'credit_and_census_data_base/credit.pkl'
 final_path = os.path.join(dir_path, file_path)
+
+import pickle
+with open(final_path, 'rb') as f:
+    x_credit_training, x_credit_test, y_credit_training, y_credit_test = pickle.load(f)
+
+print('\nVALORES TREINAMENTO:')
+print(x_credit_training.shape, y_credit_training.shape)
+print('VALORES TESTE:')
+print(x_credit_test.shape, y_credit_test.shape)
