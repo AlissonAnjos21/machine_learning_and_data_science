943a7041482ddf95ac884ec1ce8e68f11b3409e9
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-26 02:05:13 -0300

    svm_base

---
+++ svm/census.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+
+dir_path = os.path.dirname(__file__)
+file_path = 'credit_and_census_data_base/censo.pkl'
+final_path = os.path.join(dir_path, file_path)
+
+import pickle
+with open(final_path, 'rb') as f:
+    x_censo_training, x_censo_test, y_censo_training, y_censo_test = pickle.load(f)
+
+print('\nVALORES TREINAMENTO:')
+print(x_censo_training.shape, y_censo_training.shape)
+print('VALORES TESTE:')
+print(x_censo_test.shape, y_censo_test.shape)
+
+
