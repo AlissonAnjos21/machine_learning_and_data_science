522ad113588917622370377acc9864c9f071c605
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-29 21:35:25 -0300

    import

--- neural_network/credit.py
+++ neural_network/credit.py
@@ -8,3 +8,7 @@ import os
 dir_path = os.path.dirname(__file__)
 file_path = 'credit_and_census_data_base/credit.pkl'
 final_path = os.path.join(dir_path, file_path)
+
+import pickle
+with open(final_path, 'rb') as f:
+    x_credit_training, x_credit_test, y_credit_training, y_credit_test = pickle.load(f)
