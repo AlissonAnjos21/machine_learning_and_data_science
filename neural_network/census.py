739c0de1b86ed62b12ad218541bc8b795069747a
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-02 18:35:19 -0300

    load_census_data

--- neural_network/census.py
+++ neural_network/census.py
@@ -8,3 +8,7 @@ import os
 dir_path = os.path.dirname(__file__)
 file_path = 'credit_and_census_data_base/censo.pkl'
 final_path = os.path.join(dir_path, file_path)
+
+import pickle
+with open(final_path, 'rb') as f:
+    x_censo_training, x_censo_test, y_censo_training, y_censo_test = pickle.load(f)
