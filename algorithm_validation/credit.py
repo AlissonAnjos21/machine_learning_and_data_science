37976ccd5bb8fbb842ea038197a2f76d3a2591fd
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-03 19:49:06 -0300

    random_forest

--- algorithm_validation/credit.py
+++ algorithm_validation/credit.py
@@ -30,22 +30,39 @@ print('\n\n')
 # A técnica de GridSearch consiste em buscar os melhores parâmetros para serem utilizados em determinado algoritmo de machine learning e em determinada base de dados
 
 # Decision Tree:
-parameters = {
+decision_tree_parameters = {
 'criterion': ['gini', 'entropy'],
 'splitter': ['best', 'random'],
 'min_samples_split': [2, 5, 10],
 'min_samples_leaf': [1, 5, 10]
 }
-grid_search = GridSearchCV(estimator = DecisionTreeClassifier(), param_grid = parameters)  # Recebe o algoritmo de machine learning e os parâmetros que se deseja encontrar a melhor combinação
-grid_search.fit(x_credit, y_credit)
-best_parameters = grid_search.best_params_  # Informa a melhor combinação de parâmetros encontrados
-best_score = grid_search.best_score_  # Informa o melhor resultado encontrado
+decision_tree_grid_search = GridSearchCV(estimator = DecisionTreeClassifier(), param_grid = decision_tree_parameters)  # Recebe o algoritmo de machine learning e os parâmetros que se deseja encontrar a melhor combinação
+decision_tree_grid_search.fit(x_credit, y_credit)
+decision_tree_best_parameters = decision_tree_grid_search.best_params_  # Informa a melhor combinação de parâmetros encontrados
+decision_tree_best_score = decision_tree_grid_search.best_score_  # Informa o melhor resultado encontrado
 
 print('ÁRVORE DE DECISÃO:')
 print('Melhores Parâmetros Encontrados:')
-print(best_parameters)
+print(decision_tree_best_parameters)
 print('Melhor Resultado Encontrado:')
-print(best_score)
+print(decision_tree_best_score)  # 0.983
 print('\n\n')
 
+# Random Forest
+random_forest_parameters = {
+'criterion': ['gini', 'entropy'],
+'n_estimators': [10, 40, 100, 150],
+'min_samples_split': [2, 5, 10],
+'min_samples_leaf': [1, 5, 10]
+}
+random_forest_grid_search = GridSearchCV(estimator = RandomForestClassifier(), param_grid = random_forest_parameters)
+random_forest_grid_search.fit(x_credit, y_credit)
+random_forest_best_parameters = random_forest_grid_search.best_params_
+random_forest_best_score = random_forest_grid_search.best_score_
 
+print('RANDOM FOREST:')
+print('Melhores Parâmetros Encontrados:')
+print(random_forest_best_parameters)
+print('Melhor Resultado Encontrado:')
+print(random_forest_best_score)  # 0.9875
+print('\n\n')
