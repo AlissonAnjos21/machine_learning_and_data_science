7c48bea612ca942e0f00d645d2eccf377d2eb385
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-03 17:56:52 -0300

    base_credit

---
+++ algorithm_validation/credit.py
@@ -0,0 +1,18 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+import os
+
+dir_path = os.path.dirname(__file__)
+file_path = 'credit_and_census_data_base/credit.pkl'
+final_path = os.path.join(dir_path, file_path)
+
+import pickle
+with open(final_path, 'rb') as f:
+    x_credit_training, x_credit_test, y_credit_training, y_credit_test = pickle.load(f)
+
+
