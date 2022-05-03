655cc1f5e2c30c8c973bd90a86bf2291aa3ae9e3
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-03 18:04:41 -0300

    concatenate

--- algorithm_validation/credit.py
+++ algorithm_validation/credit.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -15,4 +20,5 @@ import pickle
 with open(final_path, 'rb') as f:
     x_credit_training, x_credit_test, y_credit_training, y_credit_test = pickle.load(f)
 
-
+x_credit = np.concatenate((x_credit_training, x_credit_test), axis = 0)
+y_credit = np.concatenate((y_credit_training, y_credit_test), axis = 0)
