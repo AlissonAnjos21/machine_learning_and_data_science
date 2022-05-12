e32053931fc0794ee72aef46ce3c023615fb1bc5
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-12 05:30:48 -0300

    linear_regression

--- regression/linear_regression/health_insurance.py
+++ regression/linear_regression/health_insurance.py
@@ -1,3 +1,4 @@
+from tkinter import Y
 import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -20,3 +21,26 @@ print(x_health_insurance)
 print()
 print('Y values:')
 print(y_health_insurance)
+
+print()
+print()
+print()
+
+print(np.corrcoef(x_health_insurance, y_health_insurance))  # Retorna uma matriz mostrando a correlação entre os parâmetros informados
+
+# Os valores de x estão em formato vetorial, ou seja, estão em apenas uma dimensão. Para realizar as manipulações é necessário transformá-lo em uma matriz
+
+print('\nX Shape:')
+print(x_health_insurance.shape)
+print('\nY Shape:')
+print(y_health_insurance.shape)
+
+x_health_insurance = x_health_insurance.reshape(-1, 1)  # -1: Ele ajusta com o que sobrar contanto que a condição do outro parâmetro seja cumprida
+
+print('\nReshaped X:')
+print(x_health_insurance)
+print(x_health_insurance.shape)
+
+from sklearn.linear_model import LinearRegression
+linear_regressor_health_insurance = LinearRegression()
+linear_regressor_health_insurance.fit(x_health_insurance, y_health_insurance)
