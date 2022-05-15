31a93c0d968ec9e6a371d82cd2fd4ce8504837a9
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-15 09:30:03 -0300

    mean_absolute_error

--- regression/random_forest_regression/house_price.py
+++ regression/random_forest_regression/house_price.py
@@ -17,3 +17,17 @@ y_house_price = house_price_base.iloc[:, 2].values
 
 from sklearn.model_selection import train_test_split
 x_house_price_training, x_house_price_test, y_house_price_training, y_house_price_test = train_test_split(x_house_price, y_house_price, test_size = 0.3, random_state = 0)
+
+from sklearn.ensemble import RandomForestRegressor
+random_forest_regressor = RandomForestRegressor(n_estimators = 100)  # 100 árvores
+random_forest_regressor.fit(x_house_price_training, y_house_price_training)
+predictions = random_forest_regressor.predict(x_house_price_test)
+
+from sklearn.metrics import mean_absolute_error
+print('\nMean Absolute Error:')
+print(mean_absolute_error(y_house_price_test, predictions))  # 68078.491
+
+print('\nRandom Forest Test Score:')
+print(random_forest_regressor.score(x_house_price_training, y_house_price_training))  # 0.981
+print('\nRandom Forest Test Score:')
+print(random_forest_regressor.score(x_house_price_test, y_house_price_test))  # 0.878
