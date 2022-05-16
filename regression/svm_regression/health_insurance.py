fccc29028fc77354c187952cb28781a82f852a4d
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-16 05:42:50 -0300

    normalization

--- regression/svm_regression/health_insurance.py
+++ regression/svm_regression/health_insurance.py
@@ -16,6 +16,14 @@ x_health_insurance = health_insurance_base.iloc[:, 0:1].values
 y_health_insurance = health_insurance_base.iloc[:, 1].values
 
 # Tipos de kernel: linear, polynomial, rbf
+
+# Para que o SVM fucione em sua melhor potência, é necessário normalizar os dados, pois, ao contrário de alguns outros algoritmos, ele não possui essa função pré-imbutida
+from sklearn.preprocessing import StandardScaler
+x_standard_scaler = StandardScaler()
+x_health_insurance = x_standard_scaler.fit_transform(x_health_insurance)
+y_standard_scaler = StandardScaler()
+y_health_insurance = y_standard_scaler.fit_transform(y_health_insurance.reshape(-1, 1))
+
 from sklearn.svm import SVR
 health_insurance_svm_linear_regression = SVR(kernel = 'linear')
 health_insurance_svm_linear_regression.fit(x_health_insurance, y_health_insurance)
@@ -30,14 +38,14 @@ health_insurance_svm_rbf_regression.fit(x_health_insurance, y_health_insurance)
 rbf_predictions = health_insurance_svm_rbf_regression.predict(x_health_insurance)
 
 # Gráficos:
-linear_graph = px.scatter(x = x_health_insurance.ravel(), y = y_health_insurance)
+linear_graph = px.scatter(x = x_health_insurance.ravel(), y = y_health_insurance.ravel())
 linear_graph.add_scatter(x = x_health_insurance.ravel(), y = linear_predictions, name = 'SVM  Linear Regression')
 linear_graph.show()
 
-polynomial_graph = px.scatter(x = x_health_insurance.ravel(), y = y_health_insurance)
+polynomial_graph = px.scatter(x = x_health_insurance.ravel(), y = y_health_insurance.ravel())
 polynomial_graph.add_scatter(x = x_health_insurance.ravel(), y = polynomial_predictions, name = 'SVM  Polynomial Regression')
 polynomial_graph.show()
 
-rbf_graph = px.scatter(x = x_health_insurance.ravel(), y = y_health_insurance)
+rbf_graph = px.scatter(x = x_health_insurance.ravel(), y = y_health_insurance.ravel())
 rbf_graph.add_scatter(x = x_health_insurance.ravel(), y = rbf_predictions, name = 'SVM  RBF Regression')
 rbf_graph.show()
