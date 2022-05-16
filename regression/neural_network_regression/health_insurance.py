f04a33250aa0f4f6bdc1169655c94de3e588ba27
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-16 19:47:56 -0300

    neural_network_health_insurance

--- regression/neural_network_regression/health_insurance.py
+++ regression/neural_network_regression/health_insurance.py
@@ -14,3 +14,23 @@ health_insurance_base = pd.read_csv(final_path)
 
 x_health_insurance = health_insurance_base.iloc[:, 0:1].values
 y_health_insurance = health_insurance_base.iloc[:, 1].values
+
+# Para uma maior eficácia no uso das redes neurais, também é necessário escalonar os dados
+from sklearn.preprocessing import StandardScaler
+x_standard_scaler = StandardScaler()
+x_health_insurance_scaled = x_standard_scaler.fit_transform(x_health_insurance)
+y_standard_scaler = StandardScaler()
+y_health_insurance_scaled = y_standard_scaler.fit_transform(y_health_insurance.reshape(-1, 1))
+
+from sklearn.neural_network import MLPRegressor
+health_insurance_neural_network = MLPRegressor(max_iter = 1000)
+health_insurance_neural_network.fit(x_health_insurance_scaled, y_health_insurance_scaled.ravel())
+
+print('Score:')
+print(health_insurance_neural_network.score(x_health_insurance_scaled, y_health_insurance_scaled))  # 0.962
+
+# Gráfico
+predictions = health_insurance_neural_network.predict(x_health_insurance_scaled)
+neural_network_graph = px.scatter(x = x_health_insurance_scaled.ravel(), y = y_health_insurance_scaled.ravel())
+neural_network_graph.add_scatter(x = x_health_insurance_scaled.ravel(), y = predictions, name = 'Neural Network Regression')
+neural_network_graph.show()
