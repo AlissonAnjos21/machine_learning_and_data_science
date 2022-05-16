cceeffbf36f1cc6594d93b2d4dc8b38a66e54897
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-16 05:23:32 -0300

    svm_linear_regression

--- regression/svm_regression/health_insurance.py
+++ regression/svm_regression/health_insurance.py
@@ -14,3 +14,15 @@ health_insurance_base = pd.read_csv(final_path)
 
 x_health_insurance = health_insurance_base.iloc[:, 0:1].values
 y_health_insurance = health_insurance_base.iloc[:, 1].values
+
+# Tipos de kernel: linear, polynomial, rbf
+from sklearn.svm import SVR
+health_insurance_regression = SVR(kernel = 'linear')
+health_insurance_regression.fit(x_health_insurance, y_health_insurance)
+
+predictions = health_insurance_regression.predict(x_health_insurance)
+
+# Gráfico:
+graph = px.scatter(x = x_health_insurance.ravel(), y = y_health_insurance)
+graph.add_scatter(x = x_health_insurance.ravel(), y = predictions, name = 'SVM  Linear Regression')
+graph.show()
