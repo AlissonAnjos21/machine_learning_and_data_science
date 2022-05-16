5814da23a4d554b765c2fac8160b172f4dceb170
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-16 06:21:41 -0300

    score_training_and_test_base

--- regression/svm_regression/house_price.py
+++ regression/svm_regression/house_price.py
@@ -17,3 +17,20 @@ y_house_price = house_price_base.iloc[:, 2].values
 
 from sklearn.model_selection import train_test_split
 x_house_price_training, x_house_price_test, y_house_price_training, y_house_price_test = train_test_split(x_house_price, y_house_price, test_size = 0.3, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+x_standard_scaler = StandardScaler()
+x_house_price_training_standard_scaler = x_standard_scaler.fit_transform(x_house_price_training)
+x_house_price_test_standard_scaler = x_standard_scaler.transform(x_house_price_test)
+y_standard_scaler = StandardScaler()
+y_house_price_training_standard_scaler = y_standard_scaler.fit_transform(y_house_price_training.reshape(-1, 1))
+y_house_price_test_standard_scaler = y_standard_scaler.transform(y_house_price_test.reshape(-1, 1))
+
+from sklearn.svm import SVR
+house_price_svm_regressor = SVR(kernel = 'rbf')
+house_price_svm_regressor.fit(x_house_price_training_standard_scaler, y_house_price_training_standard_scaler.ravel())
+
+print('\nScore with training base:')
+print(house_price_svm_regressor.score(x_house_price_training_standard_scaler, y_house_price_training_standard_scaler.ravel()))
+print('\nScore with test base:')
+print(house_price_svm_regressor.score(x_house_price_test_standard_scaler, y_house_price_test_standard_scaler.ravel()))
